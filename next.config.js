d0f68b14187e5b055f38cb41c3b87e0f39acb161
unknown <a22100048@mhs.stmik-sumedang.ac.id>
2026-07-24 23:21:28 +0700

    config: setup next.js static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portofolio-website',
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
   },
 }
 
